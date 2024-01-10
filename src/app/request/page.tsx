4ceeea40e4e2e745a20ded415f83e46761d1115a
Jihunee <jihun6439@daum.net>
2024-01-11 03:01:22 +0900

    [UPDATE] 받은 요청함 UI완료 및 플러팅 모달, 알럿 모달 구현 완료

--- src/app/request/page.tsx
+++ src/app/request/page.tsx
@@ -1,7 +1,12 @@
+import ReceivedRequest from '@/components/request/ReceivedRequest';
 import React from 'react';
 
 function Request() {
-  return <div>Request</div>;
+  return (
+    <div>
+      <ReceivedRequest />
+    </div>
+  );
 }
 
 export default Request;
