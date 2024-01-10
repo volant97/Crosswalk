cbc8b8ab77b47dadfc46d601692a07b178d16b9f
Hamin Lee <whereanna7@gmail.com>
2024-01-11 03:26:23 +0900

    [TIDY] 테스트코드 삭제

--- src/app/my-profile/page.tsx
+++ src/app/my-profile/page.tsx
@@ -1,20 +1,13 @@
 'use client';
 import ChooseAvatarModal from '@/components/common/modal/ChooseAvatarModal';
 import ConfirmModal from '@/components/common/modal/ConfirmModal';
-// import useConfirmModal from '@/components/common/modal/useConfirmModal';
 import React from 'react';
 
 function MyProfile() {
-  // const { openModal, confirmModal } = useConfirmModal();
   return (
     <div>
       <ConfirmModal />
-      {/* <button
-        onClick={() => {
-          openModal('확인', '바보입니까?');
-        }}
-      ></button>
-      {confirmModal('확인', '바보입니까?')} */}
+
       <ChooseAvatarModal />
     </div>
   );
