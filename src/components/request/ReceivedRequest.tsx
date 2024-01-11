afbfad0178ec3913e9e3a9e9ef8defca4268eb9c
volant97 <volant97@naver.com>
2024-01-11 18:52:18 +0900

    [UPDATE] 받은요청함 Layout 수정 및 git 문제 해결중

--- src/components/request/ReceivedRequest.tsx
+++ src/components/request/ReceivedRequest.tsx
@@ -1,40 +1,31 @@
-import { ScrollShadow } from '@nextui-org/react';
-import Link from 'next/link';
+// import { ScrollShadow } from '@nextui-org/react';
 import React from 'react';
-import { IoIosArrowRoundBack } from 'react-icons/io';
 import RequestCard from './RequestCard';
 import receiveRequestData from '@/data/receiveRequestData.json';
 
 function ReceivedRequest() {
   const { flirtingData } = receiveRequestData;
+
   return (
     <>
-      <div className="relative max-w-96 border-solid border-1 border-black px-8">
-        <header className="font-virgil max-w-80 w-full h-16 flex sticky bg-white top-0 items-center justify-center mb-8 ">
-          <Link href="/" className="absolute left-6">
-            <IoIosArrowRoundBack size="30" />
-          </Link>
-          <div className="!font-virgil ">CrossWalk</div>
-        </header>
-        <ScrollShadow size={100} hideScrollBar className="w-[300px] h-[800px]">
-          <h1 className="text-xl ml-[20px] mb-[20px]">받은 요청 {flirtingData.length}건</h1>
-          <div className="min-h-[calc(100dvh-12rem)] overflow-hidden max-h-[calc(100dvh-7rem)] overflow-y-auto scrollbar-hide">
-            {flirtingData.map((item) => {
-              return (
-                <RequestCard
-                  key={item.id}
-                  message={item.flirtingMessage}
-                  senderName={item.sender_uid}
-                  avatar={item.avatar}
-                  createdAt={item.created_at}
-                />
-              );
-            })}
-          </div>
-        </ScrollShadow>
-      </div>
+      {flirtingData.map((item) => {
+        return (
+          <RequestCard
+            key={item.id}
+            message={item.flirtingMessage}
+            senderName={item.sender_uid}
+            avatar={item.avatar}
+            createdAt={item.created_at}
+          />
+        );
+      })}
     </>
   );
 }
 
 export default ReceivedRequest;
+
+{
+  //   <ScrollShadow size={100} hideScrollBar className="w-[300px] h-[800px]"></ScrollShadow>
+  // <h1 className="flex  text-xl border-2 border-black">받은 요청 {flirtingData.length}건</h1>
+}
