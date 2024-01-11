1f1772515065661ec1bba06896dbe821b1811915
volant97 <volant97@naver.com>
2024-01-11 16:08:29 +0900

    [UPDATE] 회원등록정보 서버통신 후 recoil 저장

--- src/auth/kakao.ts
+++ src/auth/kakao.ts
@@ -4,7 +4,15 @@ import { supabase } from '@/lib/supabase-config';
 export async function kakaoLogin() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'kakao'
+    // ,
+    // options: {
+    //   queryParams: {
+    //     access_type: 'offline',
+    //     prompt: 'consent'
+    //   }
+    // }
   });
+  console.log(data);
   if (data) alert('로그인 되었습니다');
   if (error) console.error('login error : ', error);
 }
