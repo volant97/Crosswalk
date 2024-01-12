391e763c0ceab67c139146214fefeed91dd035c5
yachaechae <wpql1207@gmail.com>
2024-01-12 17:54:07 +0900

    [CSS] 피그마에 정해진 컬러셋 정의 및 기존 컬러 변경

--- src/components/register/input/Name.tsx
+++ src/components/register/input/Name.tsx
@@ -30,59 +30,66 @@ function Name() {
     }));
 
     router.push('#mbti');
-
   };
 
   return (
     <form className="min-h-[calc(100dvh-12rem)] " onSubmit={handleSubmit} id="name">
       <div className="min-h-[calc(100dvh-12rem)] flex flex-col gap-12">
         <div>
-          <h1 className=" text-[1.375rem] font-semibold">
+          <h1 className=" text-[1.375rem] font-semibold text-black">
             이름과 성별을
             <br />
             입력해주세요.
           </h1>
           <p className=" text-sm">사용자의 실명을 입력해주세요.</p>
         </div>
-        <input
-          className="h-[50px] py-[8px] px-[20px] rounded-full cursor-pointer border"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="홍길동"
-        />
+        <div className="flex flex-col gap-1">
+          <label htmlFor="inputName" className="text-gray-AAA">
+            이름
+          </label>
+          <input
+            className="h-[50px] py-[8px] px-[20px] rounded-full cursor-pointer border border-gray-DDD"
+            id="inputName"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="홍길동"
+          />
+        </div>
         {/* <Gender /> */}
-        <div className="flex gap-2 justify-center">
-          <Button
-            type="button"
-            className={`w-[48%] py-2 px-6 bg-white rounded-full cursor-pointer border ${
-              gender === 'M' ? 'font-semibold border-black text-black' : 'border-slate-300 text-slate-300'
-            }`}
-            onClick={() => {
-              setGender('M');
-            }}
-          >
-            남자
-          </Button>
-          <Button
-            type="button"
-            className={`w-[48%] py-2 px-6 bg-white rounded-full cursor-pointer border ${
-              gender === 'F' ? 'font-semibold border-black text-black' : 'border-slate-300 text-slate-300'
-            }`}
-            onClick={() => {
-              setGender('F');
-            }}
-          >
-            여자
-          </Button>
+        <div className="flex flex-col gap-1 justify-center">
+          <span className="text-gray-AAA">이름</span>
+          <div className="flex gap-2">
+            <Button
+              type="button"
+              className={`w-[48%] py-2 px-6 bg-white rounded-full cursor-pointer border ${
+                gender === 'M' ? 'font-semibold border-black text-black' : 'border-gray-DDD text-gray-AAA'
+              }`}
+              onClick={() => {
+                setGender('M');
+              }}
+            >
+              남자
+            </Button>
+            <Button
+              type="button"
+              className={`w-[48%] py-2 px-6 bg-white rounded-full cursor-pointer border ${
+                gender === 'F' ? 'font-semibold border-black text-black' : 'border-gray-DDD text-gray-AAA'
+              }`}
+              onClick={() => {
+                setGender('F');
+              }}
+            >
+              여자
+            </Button>
+          </div>
         </div>
       </div>
 
-      {/* <button className="cursor-pointer" type="submit">
-        NEXT
-      </button> */}
       <Button
-        className={`w-full rounded-3xl cursor-pointer mb-10 ${name && gender ? 'bg-customGreen' : 'bg-customYellow'}`}
+        className={`w-full font-semibold rounded-3xl text-black cursor-pointer mb-10 ${
+          name && gender ? 'bg-customGreen' : 'bg-customYellow'
+        }`}
         type="submit"
       >
         NEXT
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,8 +18,23 @@ const config: Config = {
         virgil: ['"Virgil 3 YOFF"']
       },
       colors: {
+        black: '#222',
+        customRed: '#FF1400',
         customYellow: '#FFF200',
-        customGreen: '#00E365'
+        customGreen: '#05F600',
+        lightRed: '#FF002E',
+        gray: {
+          F3: '#F3F3F3' /* F3F3F3 */,
+          EF: '#EFEFEF' /* EFEFEF */,
+          E9: '#E9E9E9' /* E9E9E9 */,
+          E6: '#E6E6E6' /* E6E6E6 */,
+          DDD: '#DDDDDD' /* DDDDDD */,
+          D9: '#D9D9D9' /* D9D9D9 */,
+          AAA: '#AAAAAA' /* AAAAAA */,
+          999: '#999999' /* 999999 */,
+          888: '#888888' /* 888888 */,
+          666: '#666666' /* 666666 */
+        }
       }
     }
   },
