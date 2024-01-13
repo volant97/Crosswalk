d69d18a2df69433db6658c86517e04faec8b7481
volant97 <volant97@naver.com>
2024-01-13 17:48:03 +0900

    [UPDATE] 마이페이지 레이아웃 작업

--- src/app/my-profile/page.tsx
+++ src/app/my-profile/page.tsx
@@ -1,15 +1,18 @@
 'use client';
 import ChooseAvatarModal from '@/components/common/modal/ChooseAvatarModal';
 import ConfirmModal from '@/components/common/modal/ConfirmModal';
+import NavBar from '@/components/common/ui/NavBar';
+import Link from 'next/link';
 import React from 'react';
+import { HiOutlineBell } from 'react-icons/hi2';
+import { IoIosArrowRoundBack } from 'react-icons/io';
 
 function MyProfile() {
   return (
-    <div>
+    <>
       <ConfirmModal />
-
       <ChooseAvatarModal />
-    </div>
+    </>
   );
 }
 
