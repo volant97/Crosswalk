3d042defaa5d5abcaf84f32b5990d30ad3adffb6
volant97 <volant97@naver.com>
2024-01-18 03:52:39 +0900

    [UPDATE] spotify 로그인 구현 성공

--- src/auth/spotify.ts
+++ src/auth/spotify.ts
@@ -12,7 +12,6 @@ export async function spotifyLogin() {
     //   }
     // }
   });
-  alert('spotify');
   console.log(data);
   if (error) console.error('login error : ', error);
 }
--- src/components/home/social_login/spotify/SpotifyLoginBtn.tsx
+++ src/components/home/social_login/spotify/SpotifyLoginBtn.tsx
@@ -1,7 +1,12 @@
+import { spotifyLogin } from '@/auth/spotify';
 import React from 'react';
 
 function SpotifyLoginBtn() {
-  return <button className="w-[3.25rem] h-[3.25rem] rounded-full bg-[#03CF5D]">A</button>;
+  return (
+    <button className="w-[3.25rem] h-[3.25rem] rounded-full bg-[#03CF5D]" type="button" onClick={spotifyLogin}>
+      S
+    </button>
+  );
 }
 
 export default SpotifyLoginBtn;
