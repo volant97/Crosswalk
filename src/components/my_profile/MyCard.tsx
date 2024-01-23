8a2194b2d6ae5b589aab0f507637ce6a61be8543
volant97 <volant97@naver.com>
2024-01-24 00:46:05 +0900

    [CSS] 마이페이지 정보 디테일

--- src/components/my_profile/MyCard.tsx
+++ src/components/my_profile/MyCard.tsx
@@ -72,14 +72,14 @@ function MyCard({ age, avatar, name, interest, height, gender, mbti }: Props) {
       <div className="flex flex-col gap-[24px] w-full bg-customGreen2  px-[20px] py-[24px]">
         <div className="flex flex-col gap-[8px]  h-[52px]">
           <h1 className="text-[18px] leading-[18px] font-[400] text-gray-999">기본정보</h1>
-          <div className="flex flex-row gap-[0.25rem] h-[26px]">
+          <div className="flex flex-row gap-[0.25rem]">
             <div
-              className={`${border} px-[12px] py-[4px] flex justify-center items-center text-[14px] font-medium leading-[14px] rounded-[20px] border-[1px] bg-customGreen2`}
+              className={`${border} flex justify-center items-center px-[10px] py-[4px] text-[13px] font-medium leading-[13px] rounded-full border-[1px] bg-customGreen2`}
             >
               {height}cm
             </div>
             <div
-              className={`${border} px-[12px] py-[4px] flex justify-center items-center text-[14px] font-medium leading-[14px]  rounded-[20px] border-[1px] bg-customGreen2`}
+              className={`${border} flex justify-center items-center px-[10px] py-[4px] text-[13px] font-medium leading-[13px] rounded-full border-[1px] bg-customGreen2`}
             >
               {gender === 'M' ? '남자' : '여자'}
             </div>
@@ -87,9 +87,9 @@ function MyCard({ age, avatar, name, interest, height, gender, mbti }: Props) {
         </div>
         <div className="flex flex-col h-[52px] ">
           <h1 className="text-[18px] leading-[18px] font-[400] text-gray-999">MBTI</h1>
-          <div className="flex flex-row gap-[0.25rem] h-[26px] mt-[0.5rem] ">
+          <div className="flex flex-row gap-[0.25rem] mt-[0.5rem] ">
             <div
-              className={`${border} flex justify-center items-center px-[12px] py-[4px]  text-[14px] font-medium leading-[14px] rounded-[20px] border-[1px] bg-customGreen2`}
+              className={`${border} flex justify-center items-center px-[10px] py-[4px] text-[13px] font-medium leading-[13px] rounded-full border-[1px] bg-customGreen2`}
             >
               {mbti}
             </div>
