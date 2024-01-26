79b9c76d4ff014906c7c501f5e46ccb719cb1670
volant97 <volant97@naver.com>
2024-01-26 15:50:57 +0900

    [FIX] build error 해결

--- src/components/request/RequestCard.tsx
+++ src/components/request/RequestCard.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
-import DeclineBtn from './Btn/DeclineBtn';
-import AcceptBtn from './Btn/AcceptBtn';
+import DeclineBtn from './btn/DeclineBtn';
+import AcceptBtn from './btn/AcceptBtn';
 
 type Props = {
   listId: number;
@@ -44,33 +44,3 @@ function RequestCard({ listId, senderId, avatar, senderName, age, message }: Pro
 }
 
 export default RequestCard;
-
-// const inputDate = new Date(createdAt);
-// const year = inputDate.getFullYear() % 100; // Extract last two digits of the year
-// const month = inputDate.getMonth() + 1; // Month is zero-based, so add 1
-// const day = inputDate.getDate();
-// const hours = inputDate.getHours();
-// const minutes = inputDate.getMinutes();
-// const formattedDate = `${year}년 ${month}월 ${day}일 ${hours}:${minutes}`;
-
-// <div className="flex justify-center">
-// <div className="flex flex-col items-center w-[280px] p-[20px] border-2 border-solid border-black mb-[20px] rounded-[20px]">
-//   <div className="w-full flex justify-between items-center mb-[10px] text-sm font-bold">
-//     이미지 : {avatar}
-//     <Image className="rounded-full" src={avatar} alt="아바타이미지" width={30} height={30} />
-//     <h1 className="ml-[-35px]">{senderName}님</h1>
-//     <h1 className="ml-[-35px]">{age}세</h1>
-//     <h1 className="text-xs text-slate-400">{formattedDate}</h1>
-//   </div>
-
-//   <h2 className="mb-[40px] text-l bold font-bold">&quot;{message}&quot;</h2>
-//   <div className="flex gap-[20px]">
-//     <Button className="hover:scale-110" color="success" variant="ghost">
-//       <BiSolidMessageCheck size={20} />
-//     </Button>
-//     <Button className="hover:scale-110" color="danger" variant="ghost">
-//       <IoClose size={20} />
-//     </Button>
-//   </div>
-// </div>
-// </div>
