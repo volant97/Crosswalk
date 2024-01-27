a7793486afef4165ed44182f661b7ba15862deb8
volant97 <volant97@naver.com>
2024-01-28 00:58:42 +0900

    [TEST]

--- src/components/request/RequestCard.tsx
+++ src/components/request/RequestCard.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
-import DeclineBtn from './dawawd/DeclineBtn';
-import AcceptBtn from './dawawd/AcceptBtn';
+import DeclineBtn from './btn/DeclineBtn';
+import AcceptBtn from './btn/AcceptBtn';
 // 여기 폴더명 변경했음
 
 type Props = {
