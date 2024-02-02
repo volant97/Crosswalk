3fd5ee41a01c160cba92d237cf3b3a1cea13a3d4
volant97 <volant97@naver.com>
2024-02-02 21:41:05 +0900

    [UPDATE] PWA 되살리기

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-// const withPlugins = require('next-compose-plugins');
-// const withPWA = require('next-pwa');
+const withPlugins = require('next-compose-plugins');
+const withPWA = require('next-pwa');
 
 const nextConfig = {
   reactStrictMode: false,
@@ -9,19 +9,19 @@ const nextConfig = {
   }
 };
 
-// module.exports = withPlugins(
-//   [
-//     [
-//       withPWA,
-//       {
-//         pwa: {
-//           dest: 'public'
-//         }
-//       }
-//     ]
-//     // 추가 플러그인 작성
-//   ],
-//   nextConfig
-// );
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: 'public'
+        }
+      }
+    ]
+    // 추가 플러그인 작성
+  ],
+  nextConfig
+);
 
 module.exports = nextConfig;
