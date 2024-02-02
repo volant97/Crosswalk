f22d1e2e10b0c163cd3be0986f1e90dc9b8f2d8a
volant97 <volant97@naver.com>
2024-02-02 18:11:32 +0900

    [UPDATE] PWA 설치가능까지 구현

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
+const withPWA = require('next-pwa');
+
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   images: {
     domains: ['opvzvzppdkhxkjybbkek.supabase.co']
   }
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: 'public'
+        }
+      }
+    ]
+    // 추가 플러그인 작성
+  ],
+  nextConfig
+);
