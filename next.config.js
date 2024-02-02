2c1fd02758b3893f4bd94d6bf98187dc964f21ab
volant97 <volant97@naver.com>
2024-02-02 19:24:08 +0900

    [TEST] PWA 삭제 (config에서도)

--- next.config.js
+++ next.config.js
@@ -1,25 +1,27 @@
 /** @type {import('next').NextConfig} */
-const withPlugins = require('next-compose-plugins');
-const withPWA = require('next-pwa');
+// const withPlugins = require('next-compose-plugins');
+// const withPWA = require('next-pwa');
 
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ['opvzvzppdkhxkjybbkek.supabase.co']
   }
 };
 
-module.exports = withPlugins(
-  [
-    [
-      withPWA,
-      {
-        pwa: {
-          dest: 'public'
-        }
-      }
-    ]
-    // 추가 플러그인 작성
-  ],
-  nextConfig
-);
+// module.exports = withPlugins(
+//   [
+//     [
+//       withPWA,
+//       {
+//         pwa: {
+//           dest: 'public'
+//         }
+//       }
+//     ]
+//     // 추가 플러그인 작성
+//   ],
+//   nextConfig
+// );
+
+module.exports = nextConfig;
