33149bd0a7e23f28ce1c7cb659c0c8e6418fb9f4
Hamin Lee <whereanna7@gmail.com>
2024-02-05 17:23:43 +0900

    [REFACTOR] chatlist에서 RenderListItem 컴포넌트로 따로 빼기, 약식 chaching 함수 추가

--- src/app/(root)/chat-list/page.tsx
+++ src/app/(root)/chat-list/page.tsx
@@ -6,18 +6,21 @@ import { fetchLastMessages, getChatList, subscribeChatList, untrackChatList } fr
 import { supabase } from '@/lib/supabase-config';
 import { UserState, userState } from '@/recoil/user';
 import { ChatListType, LastMessageDataType, MessageType } from '@/types/realTimeType';
-import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
 import useAlertModal from '@/components/common/modal/AlertModal';
+import { useRouter } from 'next/navigation';
+import React, { cache, useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+
 import { LastMessageArrayType } from '@/types/lastMessageArrayType';
 import useChatListModal from '@/components/common/modal/ChatListModal';
 import { formatDate } from '@/utils/useFormatDate';
-
+import { RenderListItem } from '@/utils/RenderListItem';
+// import { renderListItem } from '@/utils/renderListItem';
+let cachedData = [] as LastMessageArrayType;
 export default function ChatListPage() {
   const [chatList, setChatList] = useState<ChatListType[]>();
   const [getUid, setGetUid] = useRecoilState<UserState>(userState);
-  const [lastMsg, setLastMsg] = useState<LastMessageArrayType>();
+  const [lastMsg, setLastMsg] = useState<LastMessageArrayType>(cachedData);
   const router = useRouter();
   const { openModal, AlertModal } = useAlertModal();
   const { openModal: openChatListModal, AlertChatListModal } = useChatListModal();
@@ -28,11 +31,12 @@ export default function ChatListPage() {
       setChatList(chatListData);
       const roomIds = chatListData.map((item) => item.id);
       const lastMessageArray = await fetchLastMessages(roomIds);
+      cachedData = lastMessageArray;
       setLastMsg(lastMessageArray);
       // console.log('lastMsg in fetchChatListData', lastMsg);
     } catch (error) {
       console.error('error in fetchChatList', error);
-      alert('서버와의 통신을 실패했습니다.2');
+      alert('서버와의 통신을 실패했습니다.');
     }
   };
 
@@ -47,150 +51,166 @@ export default function ChatListPage() {
     };
   }, []);
 
-  const routerLink = (linkId: string, status: string) => {
-    if (status === 'ACCEPT' || status === 'SOULMATE') {
-      router.push(`/chat-list/${linkId}`);
-    } else return openChatListModal();
-  };
+  // const renderListItem = (list: ChatListType, idx: number, lastMsg: LastMessageArrayType) => {
+  //   const { openModal: openChatListModal, AlertChatListModal } = useChatListModal();
+  //   const [getUid, setGetUid] = useRecoilState<UserState>(userState);
+  //   const [chatList, setChatList] = useState<ChatListType[]>();
+  //   // const [lastMsg, setLastMsg] = useState<LastMessageArrayType>();
+  //   const isSender = getUid?.id === list.flirting_list.sender_uid.uid;
+  //   const otherUser = isSender ? list.flirting_list.receiver_uid : list.flirting_list.sender_uid;
+  //   const statusMessage = isSender ? '상대방의 수락을 기다리고 있어요.' : '회원님의 수락을 기다리고 있어요.';
+  //   const router = useRouter();
 
-  // console.log('lastMsg', lastMsg);
-  // console.log('chatList', chatList);
+  //   const routerLink = (linkId: string, status: string) => {
+  //     if (status === 'ACCEPT' || status === 'SOULMATE') {
+  //       router.push(`/chat-list/${linkId}`);
+  //     } else return openChatListModal();
+  //   };
+
+  //   return (
+  //     <li
+  //       key={idx}
+  //       className={`py-3 flex flex-row gap-0 justify-between cursor-pointer px-[20px] max-h-[68px] transition duration-300 ease-in-out hover:bg-[#FFD1E0] ${
+  //         isSender ? '' : 'px-[20px]'
+  //       } `}
+  //       onClick={() => {
+  //         if (!lastMsg) return;
+  //         if (lastMsg[idx] === null) {
+  //           return openChatListModal();
+  //         } else {
+  //           routerLink(list.id, list.flirting_list.status);
+  //         }
+  //       }}
+  //     >
+  //       <div className="flex items-center">
+  //         {ChatStatusColor(
+  //           list.flirting_list.status,
+  //           isSender ? list.flirting_list.receiver_uid.avatar : list.flirting_list.sender_uid.avatar,
+  //           otherUser.uid,
+  //           otherUser.user_img
+  //         )}
+  //       </div>
+  //       <div className="w-[12.5rem]">
+  //         <h5 className="text-black text-base font-medium">{otherUser.name}</h5>
+  //         <div className="w-full text-gray-666 text-sm font-normal text-ellipsis overflow-hidden">
+  //           {lastMsg && lastMsg[idx] !== undefined ? (
+  //             lastMsg[idx] !== null ? (
+  //               <div className="block w-full text-gray-666 text-sm whitespace-nowrap overflow-hidden text-ellipsis max-h-[20px]">
+  //                 {lastMsg[idx]?.message}
+  //               </div>
+  //             ) : (
+  //               <div className="w-full text-gray-666 text-sm text-ellipsis overflow-hidden">{statusMessage}</div>
+  //             )
+  //           ) : (
+  //             <div className="w-full text-gray-666 text-sm text-ellipsis overflow-hidden">로딩중...</div>
+  //           )}
+  //         </div>
+  //       </div>
+  //       <div className="flex flex-col justify-between items-end w-[55px]">
+  //         <span className="text-xs text-gray-AAA">
+  //           {lastMsg && lastMsg[idx] !== undefined ? (
+  //             lastMsg[idx] !== null ? (
+  //               formatDate(String(lastMsg[idx]?.created_at))
+  //             ) : (
+  //               <div className="w-full text-xs text-gray-AAA font-normal text-ellipsis overflow-hidden">대기중</div>
+  //             )
+  //           ) : (
+  //             <div className="w-full text-xs text-gray-AAA font-normal text-ellipsis overflow-hidden">...</div>
+  //           )}
+  //         </span>
+  //         {lastMsg && lastMsg[idx] && lastMsg[idx]?.user_uid !== getUid?.id && !lastMsg[idx]?.is_read && (
+  //           <div className="flex justify-center items-center w-[20px] h-[20px] font-normal rounded-full bg-customGreen3 text-white text-[11px] leading-normal">
+  //             +1
+  //           </div>
+  //         )}
+  //       </div>
+  //     </li>
+  //   );
+  // };
+
+  if (!lastMsg) {
+    return null;
+  }
+
   return (
     <Page>
       {!chatList?.length ? (
         <div className="h-screen flex items-start justify-center py-7">대화할 수 있는 방이 없어요</div>
       ) : (
-        <ul className=" ">
-          {chatList?.map((list, idx) => {
-            if (getUid?.id === list.flirting_list.sender_uid.uid) {
-              return (
-                <li
-                  key={idx}
-                  className="py-3 flex flex-row gap-0  max-h-[68px] min-h-[] justify-between cursor-pointer transition duration-300 ease-in-out hover:bg-[#FFD1E0] px-[20px]"
-                  onClick={() => {
-                    if (!lastMsg) return;
-                    if (lastMsg[idx] === null) {
-                      return openChatListModal();
-                    } else {
-                      routerLink(list.id, list.flirting_list.status);
-                    }
-                  }}
-                >
-                  <div className="flex items-center">
-                    {ChatStatusColor(
-                      list.flirting_list.status,
-                      list.flirting_list.receiver_uid.avatar,
-                      list.flirting_list.receiver_uid.uid,
-                      list.flirting_list.receiver_uid.user_img
-                    )}
-                  </div>
-                  <div className="w-[12.5rem]">
-                    <h5 className="text-black text-base font-medium">{list.flirting_list.receiver_uid.name}</h5>
-                    <div className="w-full text-gray-666 text-sm font-normal overflow-hidden ">
-                      {lastMsg && lastMsg[idx] !== undefined ? (
-                        lastMsg[idx] !== null ? (
-                          <div className="block w-full text-gray-666 text-sm whitespace-nowrap overflow-hidden text-ellipsis max-h-[20px]">
-                            {lastMsg[idx]?.message}
-                          </div>
-                        ) : (
-                          <div className="w-full text-gray-666 text-sm font-normal text-ellipsis overflow-hidden">
-                            상대방의 수락을 기다리고 있어요.
-                          </div>
-                        )
-                      ) : (
-                        <div className="w-full text-gray-666 text-sm font-normal text-ellipsis overflow-hidden">
-                          로딩중...
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                  <div className="flex flex-col  justify-between items-end w-[55px]">
-                    <span className="text-xs text-gray-AAA">
-                      {lastMsg && lastMsg[idx] !== undefined ? (
-                        lastMsg[idx] !== null ? (
-                          formatDate(String(lastMsg[idx]?.created_at))
-                        ) : (
-                          <div className="w-full text-xs text-gray-AAA  text-ellipsis overflow-hidden">대기중</div>
-                        )
-                      ) : (
-                        <div className="w-full text-xs text-gray-AAA text-ellipsis overflow-hidden">...</div>
-                      )}
-                    </span>
-                    {lastMsg && lastMsg[idx] && lastMsg[idx]?.user_uid !== getUid?.id && !lastMsg[idx]?.is_read && (
-                      <div className="flex justify-center items-center w-[20px] h-[20px] font-normal rounded-full bg-customGreen3 text-white text-[11px] leading-normal">
-                        +1
-                      </div>
-                    )}
-                  </div>
-                </li>
-              );
-            } else if (getUid?.id === list.flirting_list.receiver_uid.uid) {
-              return (
-                <li
-                  key={idx}
-                  className="py-3 flex flex-row gap-0 justify-between cursor-pointer px-[20px]  max-h-[68px] transition duration-300 ease-in-out hover:bg-[#FFD1E0]"
-                  onClick={() => {
-                    if (!lastMsg) return;
+        <ul>
+          {/* {chatList?.map((list, idx) => RenderListItem(list, idx, lastMsg))} */}
+          {chatList?.map((list, idx) => (
+            <RenderListItem list={list} idx={idx} lastMsg={lastMsg} />
+          ))}
+          {/* {chatList?.map((list, idx) => {
+            const isSender = getUid?.id === list.flirting_list.sender_uid.uid;
+            const otherUser = isSender ? list.flirting_list.receiver_uid : list.flirting_list.sender_uid;
+            const statusMessage = isSender ? '상대방의 수락을 기다리고 있어요.' : '회원님의 수락을 기다리고 있어요.';
 
-                    if (lastMsg[idx] === null) {
-                      return openChatListModal();
-                    } else {
-                      routerLink(list.id, list.flirting_list.status);
-                    }
-                  }}
-                >
-                  <div className="flex items-center">
-                    {ChatStatusColor(
-                      list.flirting_list.status,
-                      list.flirting_list.sender_uid.avatar,
-                      list.flirting_list.sender_uid.uid,
-                      list.flirting_list.sender_uid.user_img
+            return (
+              <li
+                key={idx}
+                className={`py-3 flex flex-row gap-0 justify-between cursor-pointer px-[20px] max-h-[68px] transition duration-300 ease-in-out hover:bg-[#FFD1E0] ${
+                  isSender ? '' : 'px-[20px]'
+                } `}
+                onClick={() => {
+                  if (!lastMsg) return;
+                  if (lastMsg[idx] === null) {
+                    return openChatListModal();
+                  } else {
+                    routerLink(list.id, list.flirting_list.status);
+                  }
+                }}
+              >
+                <div className="flex items-center">
+                  {ChatStatusColor(
+                    list.flirting_list.status,
+                    isSender ? list.flirting_list.receiver_uid.avatar : list.flirting_list.sender_uid.avatar,
+                    otherUser.uid,
+                    otherUser.user_img
+                  )}
+                </div>
+                <div className="w-[12.5rem]">
+                  <h5 className="text-black text-base font-medium">{otherUser.name}</h5>
+                  <div className="w-full text-gray-666 text-sm font-normal text-ellipsis overflow-hidden">
+                    {lastMsg && lastMsg[idx] !== undefined ? (
+                      lastMsg[idx] !== null ? (
+                        <div className="block w-full text-gray-666 text-sm whitespace-nowrap overflow-hidden text-ellipsis max-h-[20px]">
+                          {lastMsg[idx]?.message}
+                        </div>
+                      ) : (
+                        <div className="w-full text-gray-666 text-sm text-ellipsis overflow-hidden">
+                          {statusMessage}
+                        </div>
+                      )
+                    ) : (
+                      <div className="w-full text-gray-666 text-sm text-ellipsis overflow-hidden">로딩중...</div>
                     )}
                   </div>
-                  <div className="w-[12.5rem] ">
-                    <h5 className="text-black text-base font-medium ">{list.flirting_list.sender_uid.name}</h5>
-                    <div className="w-full text-gray-666 text-sm font-normal text-ellipsis overflow-hidden ">
-                      {lastMsg && lastMsg[idx] !== undefined ? (
-                        lastMsg[idx] !== null ? (
-                          <div className="block w-full text-gray-666 text-sm whitespace-nowrap overflow-hidden text-ellipsis max-h-[20px]">
-                            {lastMsg[idx]?.message}
-                          </div>
-                        ) : (
-                          <div className="w-full text-gray-666 text-sm text-ellipsis overflow-hidden">
-                            회원님의 수락을 기다리고 있어요.
-                          </div>
-                        )
-                      ) : (
-                        <div className="w-full text-gray-666 text-sm text-ellipsis overflow-hidden">로딩중...</div>
-                      )}
-                    </div>
-                  </div>
-                  <div className="flex flex-col justify-between items-end w-[55px]">
-                    <span className="text-xs text-gray-AAA">
-                      {lastMsg && lastMsg[idx] !== undefined ? (
-                        lastMsg[idx] !== null ? (
-                          formatDate(String(lastMsg[idx]?.created_at))
-                        ) : (
-                          <div className="w-full text-xs text-gray-AAA font-normal text-ellipsis overflow-hidden">
-                            대기중
-                          </div>
-                        )
+                </div>
+                <div className="flex flex-col justify-between items-end w-[55px]">
+                  <span className="text-xs text-gray-AAA">
+                    {lastMsg && lastMsg[idx] !== undefined ? (
+                      lastMsg[idx] !== null ? (
+                        formatDate(String(lastMsg[idx]?.created_at))
                       ) : (
                         <div className="w-full text-xs text-gray-AAA font-normal text-ellipsis overflow-hidden">
-                          ...
+                          대기중
                         </div>
-                      )}
-                    </span>
-                    {lastMsg && lastMsg[idx] && lastMsg[idx]?.user_uid !== getUid?.id && !lastMsg[idx]?.is_read && (
-                      <div className="flex justify-center items-center w-[20px] h-[20px] font-normal rounded-full bg-customGreen3 text-white text-[11px] leading-normal">
-                        +1
-                      </div>
+                      )
+                    ) : (
+                      <div className="w-full text-xs text-gray-AAA font-normal text-ellipsis overflow-hidden">...</div>
                     )}
-                  </div>
-                </li>
-              );
-            }
-          })}
+                  </span>
+                  {lastMsg && lastMsg[idx] && lastMsg[idx]?.user_uid !== getUid?.id && !lastMsg[idx]?.is_read && (
+                    <div className="flex justify-center items-center w-[20px] h-[20px] font-normal rounded-full bg-customGreen3 text-white text-[11px] leading-normal">
+                      +1
+                    </div>
+                  )}
+                </div>
+              </li>
+            );
+          })} */}
         </ul>
       )}
       {AlertModal()}
---
+++ src/utils/RenderListItem.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import { ChatListType } from '@/types/realTimeType';
+import { useRecoilState } from 'recoil';
+import useChatListModal from '@/components/common/modal/ChatListModal';
+import { UserState, userState } from '@/recoil/user';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { LastMessageArrayType } from '@/types/lastMessageArrayType';
+import { ChatStatusColor } from '@/components/message/ChatStatusColor';
+import { formatDate } from './useFormatDate';
+import { fetchLastMessages, getChatList, subscribeChatList, untrackChatList } from '@/lib/api/SupabaseApi';
+
+export const RenderListItem = ({
+  list,
+  idx,
+  lastMsg
+}: {
+  list: ChatListType;
+  idx: number;
+  lastMsg: LastMessageArrayType;
+}) => {
+  const { openModal: openChatListModal, AlertChatListModal } = useChatListModal();
+  const [getUid, setGetUid] = useRecoilState<UserState>(userState);
+  const [chatList, setChatList] = useState<ChatListType[]>();
+  // const [lastMsg, setLastMsg] = useState<LastMessageArrayType>();
+  const isSender = getUid?.id === list.flirting_list.sender_uid.uid;
+  const otherUser = isSender ? list.flirting_list.receiver_uid : list.flirting_list.sender_uid;
+  const statusMessage = isSender ? '상대방의 수락을 기다리고 있어요.' : '회원님의 수락을 기다리고 있어요.';
+  const router = useRouter();
+
+  const routerLink = (linkId: string, status: string) => {
+    if (status === 'ACCEPT' || status === 'SOULMATE') {
+      router.push(`/chat-list/${linkId}`);
+    } else return openChatListModal();
+  };
+
+  return (
+    <li
+      key={idx}
+      className={`py-3 flex flex-row gap-0 justify-between cursor-pointer px-[20px] max-h-[68px] transition duration-300 ease-in-out hover:bg-[#FFD1E0] ${
+        isSender ? '' : 'px-[20px]'
+      } `}
+      onClick={() => {
+        if (!lastMsg) return;
+        if (lastMsg[idx] === null) {
+          return openChatListModal();
+        } else {
+          routerLink(list.id, list.flirting_list.status);
+        }
+      }}
+    >
+      <div className="flex items-center">
+        {ChatStatusColor(
+          list.flirting_list.status,
+          isSender ? list.flirting_list.receiver_uid.avatar : list.flirting_list.sender_uid.avatar,
+          otherUser.uid,
+          otherUser.user_img
+        )}
+      </div>
+      <div className="w-[12.5rem]">
+        <h5 className="text-black text-base font-medium">{otherUser.name}</h5>
+        <div className="w-full text-gray-666 text-sm font-normal text-ellipsis overflow-hidden">
+          {lastMsg && lastMsg[idx] !== undefined ? (
+            lastMsg[idx] !== null ? (
+              <div className="block w-full text-gray-666 text-sm whitespace-nowrap overflow-hidden text-ellipsis max-h-[20px]">
+                {lastMsg[idx]?.message}
+              </div>
+            ) : (
+              <div className="w-full text-gray-666 text-sm text-ellipsis overflow-hidden">{statusMessage}</div>
+            )
+          ) : (
+            <div className="w-full text-gray-666 text-sm text-ellipsis overflow-hidden">로딩중...</div>
+          )}
+        </div>
+      </div>
+      <div className="flex flex-col justify-between items-end w-[55px]">
+        <span className="text-xs text-gray-AAA">
+          {lastMsg && lastMsg[idx] !== undefined ? (
+            lastMsg[idx] !== null ? (
+              formatDate(String(lastMsg[idx]?.created_at))
+            ) : (
+              <div className="w-full text-xs text-gray-AAA font-normal text-ellipsis overflow-hidden">대기중</div>
+            )
+          ) : (
+            <div className="w-full text-xs text-gray-AAA font-normal text-ellipsis overflow-hidden">...</div>
+          )}
+        </span>
+        {lastMsg && lastMsg[idx] && lastMsg[idx]?.user_uid !== getUid?.id && !lastMsg[idx]?.is_read && (
+          <div className="flex justify-center items-center w-[20px] h-[20px] font-normal rounded-full bg-customGreen3 text-white text-[11px] leading-normal">
+            +1
+          </div>
+        )}
+      </div>
+    </li>
+  );
+};
