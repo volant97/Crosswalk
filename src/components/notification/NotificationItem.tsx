e2127369992661814859f61ad8d109e6c8d7175c
Hamin Lee <whereanna7@gmail.com>
2024-02-06 12:41:44 +0900

    [REFACTOR] notificationItem 컴포넌트 분리

---
+++ src/components/notification/NotificationItem.tsx
@@ -0,0 +1,94 @@
+import Link from 'next/link';
+import { formatDate } from '@/hooks/useFormatDate';
+import { FlirtingListInNotificationType } from '@/types/flirtingListType';
+import { UserState } from '@/recoil/user';
+
+const NotificationItem = ({
+  notification,
+  currentUser,
+  userNames,
+  toggleIsReadInNoticeBoardSenderSide,
+  toggleIsReadInNoticeBoardReceiverSide
+}: {
+  notification: FlirtingListInNotificationType;
+  currentUser: UserState;
+  userNames: {
+    sender: {
+      name: string;
+      uid: string;
+    } | null;
+    receiver: {
+      name: string;
+      uid: string;
+    } | null;
+  };
+  toggleIsReadInNoticeBoardSenderSide: (id: number) => Promise<void>;
+  toggleIsReadInNoticeBoardReceiverSide: (id: number) => Promise<void>;
+}) => {
+  const senderIsRead = notification.sender_is_read_in_noti === true;
+  const receiverIsRead = notification.receiver_is_read_in_noti === true;
+  const isSender = notification.sender_uid === currentUser?.id;
+  const isReceiver = notification.receiver_uid === currentUser?.id;
+
+  if ((isSender && senderIsRead) || (isReceiver && receiverIsRead)) {
+    return null; // 숨김
+  }
+
+  if (userNames?.sender === undefined && userNames?.receiver === undefined) {
+    return (
+      <div key={notification.id} className="flex flex-col item-center max-w-96 h-18 p-2 gap-1">
+        <li className="flex flex-col item-center max-w-96 h-18 p-2 gap-1 ">신호 대기중...</li>
+      </div>
+    );
+  }
+
+  return (
+    <ul key={notification.id} className="flex-col justify-center items-center text-center">
+      <Link
+        href={
+          notification.status === 'ACCEPT'
+            ? '/chat-list'
+            : notification.status === 'UNREAD' ||
+              (notification.status === 'READ' && currentUser?.id === userNames.sender?.uid)
+            ? '/chat-list'
+            : notification.status === 'UNREAD' ||
+              (notification.status === 'READ' && currentUser?.id === userNames.receiver?.uid)
+            ? '/request'
+            : '/main'
+        }
+        className="flex flex-col item-center justify-center max-w-[100%] gap-1 cursor-pointer transition duration-300 ease-in-out hover:bg-[#FFD1E0]"
+        onClick={() => {
+          if (isSender) {
+            toggleIsReadInNoticeBoardSenderSide(notification.id);
+          } else if (isReceiver) {
+            toggleIsReadInNoticeBoardReceiverSide(notification.id);
+          }
+        }}
+      >
+        <li className="flex flex-col justify-center w-full h-[70px] pl-[22px] pr-[22px] pt-[13px] pb-[13px] cursor-pointer gap-[5px]">
+          <div className="flex justify-between">
+            <div className="flex flex-col justify-center items-center text-center font-medium leading-[16px] h-[20px] text-[16px] ">
+              {notification.status === 'ACCEPT' ? (
+                <h2 className="flex flex-col justify-center ">💚 Connected!</h2>
+              ) : (
+                <h2>⚡ Request</h2>
+              )}
+            </div>
+            <p className="text-right font-Pretendard text-xs font-normal leading-none text-[#AAA] text-[12px] ">
+              {formatDate(String(notification.created_at))}
+            </p>
+          </div>
+          <div className="flex flex-row overflow-hidden  truncate ">
+            <p className="text-gray-666 text-[14px] text-Pretendard font-normal leading-[19px]">
+              {notification.status === 'ACCEPT'
+                ? `${userNames.sender?.name}님과 ${userNames.receiver?.name}님의 신호등이 연결되었습니다!`
+                : `${userNames.sender?.name}님이 ${userNames.receiver?.name}님에게 연결 요청을 보냈습니다.`}
+            </p>
+          </div>
+        </li>
+      </Link>
+    </ul>
+  );
+};
+
+export default NotificationItem;
--- src/components/notification/NotificationList.tsx
+++ src/components/notification/NotificationList.tsx
@@ -1,27 +1,13 @@
 'use client';
 import { Fragment, useEffect, useState } from 'react';
-import {
-  getNotificationDetail,
-  getUser1NameNotification,
-  getUser2NameNotification,
-  subscribeFlirtingList,
-  updateIsReadInNotiReceiverSide,
-  updateIsReadInNotiSenderSide
-} from '@/lib/api/SupabaseApi';
+import { getUser1NameNotification, getUser2NameNotification, subscribeFlirtingList } from '@/lib/api/SupabaseApi';
 import useAlertModal from '@/components/common/modal/AlertModal';
-import type {
-  FlirtingListInNotificationType,
-  FlirtingListType,
-  customUserNameNotiType
-} from '@/types/flirtingListType';
-import Link from 'next/link';
 import { useRecoilState } from 'recoil';
 import { userState } from '@/recoil/user';
-import { formatDate } from '@/hooks/useFormatDate';
 import useFetchNotificationData from '@/hooks/useFetchNotificationData';
-import useFetchUserNamesInNotiBell from '@/hooks/useFetchUserNamesNotiBell';
 import useToggleIsReadInNoticeBoard from '@/hooks/useToggleIsReadInNotiList';
-// import useFetchUserNamesNotiList from '@/hooks/useFetchUserNamesInNotiList';
+import NotificationItem from './NotificationItem';
+import type { FlirtingListInNotificationType } from '@/types/flirtingListType';
 
 const NotificationList = () => {
   const { openModal } = useAlertModal();
@@ -30,7 +16,6 @@ const NotificationList = () => {
   const [userNames, setUserNames] = useState<
     { sender: { name: string; uid: string } | null; receiver: { name: string; uid: string } | null }[]
   >([]);
-
   const fetchNotificationData = useFetchNotificationData(setNotificationData, openModal);
   const { toggleIsReadInNoticeBoardSenderSide, toggleIsReadInNoticeBoardReceiverSide } =
     useToggleIsReadInNoticeBoard(openModal);
@@ -44,14 +29,6 @@ const NotificationList = () => {
     fetchNotificationData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  // useFetchUserNamesNotiList(
-  //   notificationData,
-  //   setUserNames,
-  //   getUser1NameNotification,
-  //   getUser2NameNotification,
-  //   openModal
-  // );
 
   useEffect(() => {
     const fetchUserNames = async () => {
@@ -85,27 +62,6 @@ const NotificationList = () => {
     }
   }, [notificationData]);
 
-  // const toggleIsReadInNoticeBoardSenderSide = async (id: number | null) => {
-  //   try {
-  //     if (id !== null) {
-  //       await updateIsReadInNotiSenderSide(id);
-  //     }
-  //   } catch (error) {
-  //     openModal('알림을 읽는 중에 오류가 발생했습니다.');
-  //   }
-  // };
-
-  // const toggleIsReadInNoticeBoardReceiverSide = async (id: number | null) => {
-  //   try {
-  //     if (id !== null) {
-  //       await updateIsReadInNotiReceiverSide(id);
-  //     }
-  //   } catch (error) {
-  //     openModal('알림을 읽는 중에 오류가 발생했습니다.');
-  //   }
-  // };
-  // console.log('notificcationDATAAA:', notificationData);
-
   return (
     <Fragment>
       <div className="flex-col items-center justify-center w-[100%]  min-h-[calc(100dvh-12rem)] overflow-hidden max-h-[calc(100dvh-7rem)] overflow-y-auto scrollbar-hide">
@@ -117,69 +73,16 @@ const NotificationList = () => {
               <li className="flex flex-col item-center w-full h-18 p-2 gap-1 text-center">알림을 모두 읽었어요</li>
             </div>
           ) : (
-            notificationData?.map((notification, index) => {
-              const senderIsRead = notification.sender_is_read_in_noti === true;
-              const receiverIsRead = notification.receiver_is_read_in_noti === true;
-              const isSender = notification.sender_uid === currentUser?.id;
-              const isReceiver = notification.receiver_uid === currentUser?.id;
-              if ((isSender && senderIsRead) || (isReceiver && receiverIsRead)) {
-                return null; // 숨김
-              }
-              if (userNames[index]?.sender === undefined && userNames[index]?.receiver === undefined) {
-                return (
-                  <div key={notification.id} className="flex flex-col item-center max-w-96 h-18 p-2 gap-1">
-                    <li className="flex flex-col item-center max-w-96 h-18 p-2 gap-1 ">신호 대기중...</li>
-                  </div>
-                );
-              }
-              return (
-                <ul key={notification.id} className="flex-col justify-center items-center text-center">
-                  <Link
-                    href={
-                      notification.status === 'ACCEPT'
-                        ? '/chat-list'
-                        : notification.status === 'UNREAD' ||
-                          (notification.status === 'READ' && currentUser?.id === userNames[index]?.sender?.uid)
-                        ? '/chat-list'
-                        : notification.status === 'UNREAD' ||
-                          (notification.status === 'READ' && currentUser?.id === userNames[index]?.receiver?.uid)
-                        ? '/request'
-                        : '/main'
-                    }
-                    className="flex flex-col item-center justify-center max-w-[100%] gap-1 cursor-pointer transition duration-300 ease-in-out hover:bg-[#FFD1E0]"
-                    onClick={() => {
-                      if (isSender) {
-                        toggleIsReadInNoticeBoardSenderSide(notification.id);
-                      } else if (isReceiver) {
-                        toggleIsReadInNoticeBoardReceiverSide(notification.id);
-                      }
-                    }}
-                  >
-                    <li className="flex flex-col justify-center w-full h-[70px] pl-[22px] pr-[22px] pt-[13px] pb-[13px] cursor-pointer gap-[5px]">
-                      <div className="flex justify-between">
-                        <div className="flex flex-col justify-center items-center text-center font-medium leading-[16px] h-[20px] text-[16px] ">
-                          {notification.status === 'ACCEPT' ? (
-                            <h2 className="flex flex-col justify-center ">💚 Connected!</h2>
-                          ) : (
-                            <h2>⚡ Request</h2>
-                          )}
-                        </div>
-                        <p className="text-right font-Pretendard text-xs font-normal leading-none text-[#AAA] text-[12px] ">
-                          {formatDate(String(notification.created_at))}
-                        </p>
-                      </div>
-                      <div className="flex flex-row overflow-hidden  truncate ">
-                        <p className="text-gray-666 text-[14px] text-Pretendard font-normal leading-[19px]">
-                          {notification.status === 'ACCEPT'
-                            ? `${userNames[index]?.sender?.name}님과 ${userNames[index]?.receiver?.name}님의 신호등이 연결되었습니다!`
-                            : `${userNames[index]?.sender?.name}님이 ${userNames[index]?.receiver?.name}님에게 연결 요청을 보냈습니다.`}
-                        </p>
-                      </div>
-                    </li>
-                  </Link>
-                </ul>
-              );
-            })
+            notificationData?.map((notification, index) => (
+              <NotificationItem
+                key={notification.id}
+                notification={notification}
+                currentUser={currentUser}
+                userNames={userNames[index]}
+                toggleIsReadInNoticeBoardSenderSide={toggleIsReadInNoticeBoardSenderSide}
+                toggleIsReadInNoticeBoardReceiverSide={toggleIsReadInNoticeBoardReceiverSide}
+              />
+            ))
           )
         ) : (
           <div className="flex flex-col item-center justify-center w-full h-18 p-[20px] gap-1 ">
