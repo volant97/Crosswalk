663d741af28d999116226b1670003b17686bb36b
volant97 <volant97@naver.com>
2024-02-06 15:02:44 +0900

    [CSS] 랜딩페이지 dvh 적용

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -6,7 +6,7 @@ import login_main_img from '@assets/login/login_main_img.png';
 
 export default function HomePage() {
   return (
-    <div className="relative flex flex-col items-center justify-evenly h-screen">
+    <div className="relative flex flex-col items-center justify-evenly h-[100dvh]">
       <header className="flex flex-col gap-[14px] items-center text-black">
         <h1 className="w-[140px] h-[32px] text-[28px] font-virgil font-[600] leading-normal">Crosswalk</h1>
         <h2 className="text-[18px] font-pretendard font-[400] leading-normal">인생의 소울메이트를 만나보세요!</h2>
