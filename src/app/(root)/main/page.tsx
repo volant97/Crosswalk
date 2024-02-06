ba9ea03e8c94d065c3f420acec95cefa29c3ba66
volant97 <volant97@naver.com>
2024-02-06 15:12:53 +0900

    [TIDY] 메인, 불필요한 Layout 파일 삭제

--- src/app/(root)/main/page.tsx
+++ src/app/(root)/main/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Page from '@/components/layout/Page';
-import NavBar from '@/components/common/ui/NavBar';
 import UserCards from '@/components/main/UserCards';
 
 //오류해결하기!
