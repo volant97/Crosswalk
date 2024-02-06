0738e323aed7e618e44dd55e3058e4e9e97d411c
volant97 <volant97@naver.com>
2024-02-06 18:01:56 +0900

    [TEST] 유저 정보 삭제 공지 제작중

--- src/app/(root)/main/page.tsx
+++ src/app/(root)/main/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Page from '@/components/layout/Page';
 import UserCards from '@/components/main/UserCards';
+import DataDeletionNotice from '@/components/main/data-deletion-notice/DataDeletionNotice';
 
 function MainPage() {
   return (
     <Page noBack>
       <UserCards />
+      <DataDeletionNotice />
     </Page>
   );
 }
---
+++ src/components/main/data-deletion-notice/DataDeletionNotice.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function DataDeletionNotice() {
+  return (
+    <div className="absolute z-200 top-[50%] left-[50%]">
+      <div>안내임</div>
+      <div>안내임</div>
+      <div>안내임</div>
+    </div>
+  );
+}
+
+export default DataDeletionNotice;
