f5a7f3d983646082bac6bc32d1ab0fea247762ee
volant97 <volant97@naver.com>
2024-02-08 10:52:54 +0900

    [UPDATE] register dvh

--- src/app/(root)/register/layout.tsx
+++ src/app/(root)/register/layout.tsx
@@ -5,7 +5,7 @@ import Indicator from '@/components/register/IndicatorScroll';
 function RegisterLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col items-center">
-      <div className="h-screen w-full max-w-[430px] min-w-[360px] border-x !overflow-y-hidden overflow-x-hidden scrollbar-hide">
+      <div className="h-[100dvh] w-full max-w-[430px] min-w-[360px] border-x !overflow-y-hidden overflow-x-hidden scrollbar-hide">
         <header className="relative flex flex-col justify-center font-virgil h-[9dvh] top-0 bg-white items-center border-[#E9EAEB] ">
           <Link href={'/'}>
             <div className="flex items-center justify-center w-full h-full pt-[5px] text-[3dvh] font-virgil font-[500]">
